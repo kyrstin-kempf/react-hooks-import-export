886795f521f1a10fea079756ba6a04849eae5a4b
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-04-17 14:20:27 -0400

    commit tree 8e45568ee655acdf3abb1b4e76ea0f266505c5b5

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  );
 }
+console.log(trees);
+wildlife();
+
+console.log(RMFunctions.trees);
+RMFunctions.wildlife();
+
+export default ColoradoStateParks;
